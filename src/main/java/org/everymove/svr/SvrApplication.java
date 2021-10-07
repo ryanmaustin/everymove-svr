6081a99c1cabb848330cb5ad70df2182cf076b77
Ryan Austin <rmaustin@aurora.lab>
2021-10-07 13:21:07 -0700

    Initial Spring Boot setup

---
+++ src/main/java/org/everymove/svr/SvrApplication.java
@@ -0,0 +1,13 @@
+package org.everymove.svr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/everymove/svr/SvrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.everymove.svr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
